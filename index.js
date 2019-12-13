27140290d209c59376ce661eb9124e33b5be2803
Sourabh Saha <sousaha@amazon.com>
2019-12-12 18:09:01 -0800

    added game restart and speed up character

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ var config = {
 
 var game = new Phaser.Game(config);
 var score = 0, scoreText;
+var gameOver;
+var PLAYER_VELOCITY_X = 250;
+var PLAYER_VELOCITY_Y = 500;
+var PLAYER_START_X = 100;
+var PLAYER_START_Y = 450;
 
+// preloads the assets with key-value pairing
 function preload ()
 {
     this.load.image('sky','./assets/sky.png');
@@ -30,36 +36,38 @@ function preload ()
     );  
 }
 
+// creates the scene, run only once
 function create ()
 {
     this.add.image(400,300,'sky');
 
+// platform
     platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
     platforms.create(600,400,'ground');
     platforms.create(50,250,'ground');
     platforms.create(750,220,'ground');
 
+// stars
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
         setXY: { x: 12, y: 0, stepX:70 }
     });
-
     stars.children.iterate(function (child) {
         child.setBounceY(Phaser.Math.FloatBetween(0.4,0.8));
     });
 
+// bomb
     bombs = this.physics.add.group();
 
-    player = this.physics.add.sprite(100,450,'dude');
-
+// player
+    player = this.physics.add.sprite(PLAYER_START_X,PLAYER_START_Y,'dude');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
     player.body.setGravityY(300);
 
+// animations
     this.anims.create({
         key:'left',
         frames: this.anims.generateFrameNumbers('dude',{start: 0, end: 3}),
@@ -80,17 +88,23 @@ function create ()
         repeat: -1
     });
 
+// colliders
     this.physics.add.collider(player,platforms);
     this.physics.add.collider(stars, platforms);
-    this.physics.add.overlap(player, stars, collectStar, null, this);
     this.physics.add.collider(bombs, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
     this.physics.add.overlap(player, bombs, hitBomb, null, this);
 
+// input
     cursors = this.input.keyboard.createCursorKeys();
+    this.input.keyboard.on('keydown-R', restartGame, this);
 
-    scoreText = this.add.text(16, 16, 'score:0', {fontSize: '32px', fill: '#000'});
+// score
+    scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
+    gameOver = false;
 }
 
+// callback to execute when star is collected
 function collectStar (player, star)
 {
     star.disableBody(true, true);
@@ -112,6 +126,7 @@ function collectStar (player, star)
     }
 }
 
+// callback to execute when bomb is touched
 function hitBomb (player, bomb)
 {
     this.physics.pause();
@@ -120,29 +135,41 @@ function hitBomb (player, bomb)
     gameOver = true;
 }
 
+// callback to restart the game
+function restartGame (event){
+    if (gameOver) 
+    {
+        score = 0;
+        this.scene.restart();
+    }
+}
+
+// update loop [Main game loop]
 function update ()
 {
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-160);
-
+        player.setVelocityX(-PLAYER_VELOCITY_X);
         player.anims.play('left', true);
     }
     else if (cursors.right.isDown)
     {
-        player.setVelocityX(160);
-
+        player.setVelocityX(PLAYER_VELOCITY_X);
         player.anims.play('right', true);
     }
     else
     {
         player.setVelocityX(0);
-
         player.anims.play('turn');
     }
 
     if (cursors.up.isDown && player.body.touching.down)
     {
-        player.setVelocityY(-500);
+        player.setVelocityY(-PLAYER_VELOCITY_Y);
+    }
+
+    if (gameOver === true)
+    {
+        scoreText.setText('Final Score is ' + score + ' Press R to Restart');
     }
 }
